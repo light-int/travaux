68da0f097e54b2641d85a7e4f15196fc3ed0112e
light-int <44721942+light-int@users.noreply.github.com>
2021-11-24 11:50:39 +0100

    Update API key

--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -4,7 +4,7 @@ const Axios = axios.create({
   baseURL: "https://api.airtable.com/v0/appnO0cmp8RM55Ai7/projets"
 });
 
-Axios.defaults.headers.common = {'Authorization': 'Bearer keyNTYiBHFVxakdjQ'}
+Axios.defaults.headers.common = {'Authorization': 'Bearer keyQ5sI1ohgJYlbf2'}
 
 export default{
     getProjects() {
